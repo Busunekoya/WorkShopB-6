96cd9fa99c6be58fc5ce66492b7786aaccfe9794
Busunekoya <92299420+Busunekoya@users.noreply.github.com>
2025-07-01 16:24:52 +0900

    だるまさんが転んだでプレイヤーの人数を設定できるようになりました

--- だるまさんがころんだ.py
+++ だるまさんがころんだ.py
@@ -1,1 +1,25 @@
-print("Helloworld")
+"""import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25, GPIO.OUT)
+
+gameset = 0
+
+
+
+def darmagame():
+    if gameset == 0:
+        GPIO.output(25, GPIO.HIGH)
+        print('light ON')
+        GPIO.output(25, GPIO.LOW)
+"""
+player_num = int(input("プレイヤーの人数を入力してください"))
+print(player_num)
+
+boolplayer = []
+
+for i in range(player_num):
+    boolplayer.append(0)
+
+print(boolplayer)
